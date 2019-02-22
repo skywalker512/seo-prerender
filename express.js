a8c7e502a735162b809f06b6ae7a508531679b23
skywalker512 <houzhenhong@hotmail.com>
2019-02-22 23:08:41 +0800

    [提交] 去掉 redis

--- express.js
+++ express.js
@@ -1,25 +1,25 @@
 const express = require('express')
 const app = express()
-const redis = require("redis"),
-client = redis.createClient({ url: process.env.REDIS  }),
-cacheableStatusCodes = { 200: true, 302: true, 404: true }
+// const redis = require("redis"),
+// client = redis.createClient({ url: process.env.REDIS  }),
+// cacheableStatusCodes = { 200: true, 302: true, 404: true }
 
 app.use(require('prerender-node')
     .set('protocol', process.env.HOST_PROTOCOL)
     .set('host', process.env.HOST_URL)
     .set('prerenderToken', process.env.TOKEN)
     .set('forwardHeaders', true)
-    .set('beforeRender', function (req, done) {
-        client.hmget(req.url, 'body', 'status', function (err, fields) {
-            if (err) return done(err);
-            done(err, { body: fields[0], status: fields[1] });
-        });
-    })
-    .set('afterRender', function (err, req, prerender_res) {
-        if (cacheableStatusCodes[prerender_res.statusCode]) {
-            client.hmset(req.url, 'body', prerender_res.body, 'status', prerender_res.statusCode);
-        }
-    })
+    // .set('beforeRender', function (req, done) {
+    //     client.hmget(req.url, 'body', 'status', function (err, fields) {
+    //         if (err) return done(err);
+    //         done(err, { body: fields[0], status: fields[1] });
+    //     });
+    // })
+    // .set('afterRender', function (err, req, prerender_res) {
+    //     if (cacheableStatusCodes[prerender_res.statusCode]) {
+    //         client.hmset(req.url, 'body', prerender_res.body, 'status', prerender_res.statusCode);
+    //     }
+    // })
 )
 
 
